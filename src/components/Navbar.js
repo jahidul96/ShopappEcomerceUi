719b1751b06b347b3df8e5cf59e3b186554163a8
jahidul islam <Jahidul96@yahoo.com>
2022-07-16 11:43:46 +0600

    added some code

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,78 +1,82 @@
-import './styles/nav.css'
-import { BiCart, BiSearch } from 'react-icons/bi'
-import { AiOutlineMenu } from 'react-icons/ai'
-import { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import "./styles/nav.css";
+import {BiCart, BiSearch} from "react-icons/bi";
+import {AiOutlineMenu} from "react-icons/ai";
+import {useState} from "react";
+import {useNavigate} from "react-router-dom";
 
 export default function Navbar() {
+	const [showNav, setShowNav] = useState(false);
 
-    const [showNav, setShowNav] = useState(false)
+	const navigate = useNavigate();
 
-    const navigate = useNavigate()
+	const gotoHomePage = () => {
+		navigate("/");
+	};
 
-    const gotoHomePage = () => {
-        navigate('/')
-    }
+	const toggleNav = () => {
+		setShowNav(!showNav);
+	};
 
-    const toggleNav = () => {
-        setShowNav(!showNav)
+	return (
+		<>
+			<div className="navbar">
+				<div className="container navItemWrapper">
+					<div className="navLogoDiv" onClick={gotoHomePage}>
+						<h2>Irian's_Shop</h2>
+					</div>
 
-    }
+					<div className="navMiddleDiv">
+						<div>
+							<input
+								className="navbarInput"
+								placeholder="search..."
+							/>
+							<span className="searchBtn">
+								<BiSearch size={20} color={"white"} />
+							</span>
+						</div>
+					</div>
+					<div className="navRightDiv">
+						<div className="cartIcon">
+							<div>
+								<span>
+									<BiCart size={30} />
+								</span>
+								<span className="cartItemCounter">1</span>
+							</div>
+						</div>
 
-    return (
-        <>
+						<span className="downoadapp">DownloadApp</span>
+					</div>
 
+					<div className="menuIcon" onClick={toggleNav}>
+						<AiOutlineMenu size={25} color="#000" />
+					</div>
+				</div>
+			</div>
 
-            <div className='navbar'>
-                <div className='container navItemWrapper'>
-                    <div className='navLogoDiv' onClick={gotoHomePage}>
-                        <h2>ShopApp</h2>
-                    </div>
+			{/* small screen search input */}
+			{showNav && (
+				<div className="smallScreenNavigationStyles">
+					<div className="smallScreenSearch ">
+						<input
+							className="navbarInput navSmallSearchInput"
+							placeholder="search..."
+						/>
+						<button className="smallScreenDownloadBtn">
+							DownloadApp
+						</button>
+					</div>
 
-                    <div className='navMiddleDiv'>
-                        <div>
-                            <input className='navbarInput' placeholder='search...' />
-                            <span className='searchBtn'><BiSearch size={20} color={'white'} /></span>
-                        </div>
-                    </div>
-                    <div className='navRightDiv'>
-                        <div className='cartIcon'>
-                            <div>
-                                <span ><BiCart size={30} /></span>
-                                <span className='cartItemCounter'>1</span>
-                            </div>
-                        </div>
-
-                        <span className='downoadapp'>DownloadApp</span>
-                    </div>
-
-                    <div className='menuIcon' onClick={toggleNav}>
-                        <AiOutlineMenu size={25} color="#000" />
-                    </div>
-                </div>
-            </div>
-
-            {/* small screen search input */}
-            {
-                showNav && (
-                    <div className='smallScreenNavigationStyles'>
-                        <div className='smallScreenSearch '>
-                            <input className='navbarInput navSmallSearchInput' placeholder='search...' />
-                            <button className='smallScreenDownloadBtn'>DownloadApp</button>
-                        </div>
-
-                        <div className='smallScreenPrenav'>
-                            <button>Save more on App</button>
-                            <button>Sell on Shop</button>
-                            <button>Customer Care</button>
-                            <button>Singin/Login</button>
-                            <button>Tearms & Policy</button>
-                        </div>
-                    </div>
-                )
-            }
-
-
-        </>
-    )
+					<div className="smallScreenPrenav">
+						<button>Save more on App</button>
+						<button>Sell on Shop</button>
+						<button>Customer Care</button>
+						<button>Singin/Login</button>
+						<button>Tearms & Policy</button>
+					</div>
+				</div>
+			)}
+		</>
+	);
 }
--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,30 +1,30 @@
-import './styles/slide.css'
+import "./styles/slide.css";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
-import { Carousel } from 'react-responsive-carousel';
-import { Link, useNavigate } from 'react-router-dom'
+import {Carousel} from "react-responsive-carousel";
+import {Link, useNavigate} from "react-router-dom";
 
 export default function Slider() {
-    const navigate = useNavigate()
+	const navigate = useNavigate();
 
-    const gotoDetailsPage = () => {
-        navigate('/product/productdetails')
-    }
-    return (
-        <Carousel
-            autoPlay={true}
-            showThumbs={false}
-            showArrows={false}
-            className='slider'>
-            <div className='sliderItemWrapper' onClick={gotoDetailsPage}>
-                <img src="https://img.freepik.com/free-psd/fashion-banner-template_23-2148509060.jpg?w=2000" />
-            </div>
-            <div className='sliderItemWrapper' onClick={gotoDetailsPage}>
-                <img src="https://img.freepik.com/free-psd/horizontal-banner-template-big-sale-with-woman-shopping-bags_23-2148786755.jpg?w=2000" />
-            </div>
-            <div className='sliderItemWrapper' onClick={gotoDetailsPage}>
-                <img src="https://img.freepik.com/free-vector/fashion-sale-banners-with-photo_52683-9828.jpg?w=2000" />
-            </div>
-
-        </Carousel>
-    )
+	const gotoDetailsPage = () => {
+		navigate("/product/productdetails");
+	};
+	return (
+		<Carousel
+			autoPlay={true}
+			showThumbs={false}
+			showArrows={false}
+			className="slider"
+		>
+			<div className="sliderItemWrapper" onClick={gotoDetailsPage}>
+				<img src="https://img.freepik.com/free-vector/fashion-sale-banners-with-photo_52683-9828.jpg?w=2000" />
+			</div>
+			<div className="sliderItemWrapper" onClick={gotoDetailsPage}>
+				<img src="https://img.freepik.com/free-psd/horizontal-banner-template-big-sale-with-woman-shopping-bags_23-2148786755.jpg?w=2000" />
+			</div>
+			<div className="sliderItemWrapper" onClick={gotoDetailsPage}>
+				<img src="https://img.freepik.com/free-psd/fashion-banner-template_23-2148509060.jpg?w=2000" />
+			</div>
+		</Carousel>
+	);
 }
